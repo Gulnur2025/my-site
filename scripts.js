504a822d47eadb146879c1ba95f2cb478ade9fb4
Gulnur2025 <marshalldavu3@gmail.com>
2025-03-14 23:10:41 +0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hexagons = document.querySelectorAll(".hex");
-    hexagons.forEach(hex => {
-        hex.addEventListener("click", function() {
-            window.location.href = this.querySelector("a").href;
-        });
-    });
-});
